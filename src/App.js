f86a65af87a2519eb6556bf6e9e4e9b2454ba156
Nithya Subramaniyan <nithya.subramaniyan@itp.objectfrontier.com>
2024-03-14 09:44:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,148 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  //States for Register
+  const[firstName,setFirstName] = useState("")
+  const[lastName,setLastName] = useState("")
+  const[email,setEmail] = useState("")
+  const[password,setPassword] = useState("")
+  const[confirmPassword,setconfirmPassword] = useState("")
+  
+  //Validation
+  function validateForm()
+  {
+    if (firstName.length == 0)
+    {
+      alert('First Name Should not be Empty')
+      return
+    }
+
+    if (email.length == 0) {
+      alert('Email Address should not be empty')
+      return
+    }
+
+    if (password.length < 8) {
+      alert('Password must contain minimum 8 characters')
+      return
+    }
+
+    if (password.length > 14) {
+      alert('Password should contain maximum 14 characters')
+      return
+    }
+
+    let countUpperCase = 0   
+    let countLowerCase = 0
+    let countDigit = 0
+    let countSpecialCharacters = 0
+
+    for (let i = 0; i < password.length; i++) {
+      const specialChars = [
+        '!','@','#','$',
+        '%',
+        '^',
+        '&',
+        '*',
+        '(',
+        ')',
+  '_',
+        '-','+',
+        '=','[','{',
+        ']','}',
+        ':',';',
+        '<','>',
+      ]
+
+      if (specialChars.includes(password[i])) {
+        countSpecialCharacters++
+      } else if (!isNaN(password[i] * 1)) {
+        countDigit++
+      } else {
+        if (password[i] == password[i].toUpperCase()) {
+          countUpperCase++
+        }
+        if (password[i] == password[i].toLowerCase()) {
+          countLowerCase++
+        }
+      }
+    }
+
+
+  }
+
+  //Handling the Name Change
+
+  const handleFirstName = (e) => {
+    setFirstName(e.target.value);
+  };
+
+  //alertbox
+  // const alertbox = () =>
+  // {
+  //   alert("Register Successfully..!");
+  // }
+
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="base-container">
+      {/* <h1>Welcome</h1> */}
+      <br/>
+      <div className="Form">
+      <h2 data-testid="Heading">Sign Up</h2>
+      <br/>
+      <input type="text" data-testid="TxtFirstName" placeholder="First Name" onChange={handleFirstName} />
+      <br/>
+      <input type="text" data-testid="TxtLastName" placeholder="Last Name" onChange={(e) => setLastName(e.target.value)} />
+      <br/>
+      <input type="email" data-testid="TxtEmail" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <br/>
+      <input type="password" data-testid="TxtPassword" placeholder="Password"  onChange={(e) => setPassword(e.target.value)} />
+      <br/>
+      <input type="password" data-testid="TxtConfirmPassword" placeholder="Confirm Password"  onChange={(e) => setconfirmPassword(e.target.value)} />
+      <br/>
+      <input className="submit" data-testid="RegisterButton" type="submit" value="Register" onClick={() => { validateForm() }} />
+    </div>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+// const [formData, setFormData] = useState({
+//   firstname: "",
+//   lastname: "",
+//   email: "",
+//   password: "",
+//   confirmpassword: ""
+// });
+
+
+ // const CreateRegister = () => {
+    //   alert("Registered Successfully..!");
+    // }
+
+
+
+
+
+
+
+{/* <label>First Name :</label> <input type="text" placeholder="First Name" />
+<br/>
+<label>Last Name :</label> <input type="text" placeholder="Last Name" />
+<br/>
+<label>Email :</label> <input type="email" placeholder="Email" />
+<br/>
+<label>Password :</label> <input type="password" placeholder="Password" />
+<br/>
+<label>Confirm Password :</label> <input type="password" placeholder="Confirm Password" />
+<br/>
+<input type="submit" value="Register" /> */}
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,71 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe('My Calculator Test Suite', () => {
+  test('Test Case-1[Render All UI Components]',()=>{
+    render(<App/>);
+    const txtFirstName = screen.getByTestId('TxtFirstName');
+    const txtLastName = screen.getByTestId('TxtLastName');
+    const txtEmail = screen.getByTestId('TxtEmail');
+    const txtPassword = screen.getByTestId('TxtPassword');
+    const txtConfirmPassword = screen.getByTestId('TxtConfirmPassword');
+    const registerButton = screen.getByTestId('RegisterButton');
+  
+    expect(txtFirstName).toBeInTheDocument();
+    expect(txtLastName).toBeInTheDocument();
+    expect(txtEmail).toBeInTheDocument();
+    expect(txtPassword).toBeInTheDocument();
+    expect(txtConfirmPassword).toBeInTheDocument();
+    expect(registerButton).toBeInTheDocument();
+  });
+
+  test('Test Case-2[Render All Text Message]',()=> {
+    render(<App/>);
+    const heading = screen.getByTestId("Heading");
+
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent("Sign Up");
+
+  });
+
+  test('Test Case-3 [Testing Texboxes Initial Value]',() =>{
+    render(<App/>);
+    const txtFirstName = screen.getByTestId("TxtFirstName");
+    const txtLastName = screen.getByTestId('TxtLastName');
+    const txtEmail = screen.getByTestId('TxtEmail');
+    const txtPassword = screen.getByTestId('TxtPassword');
+    const txtConfirmPassword = screen.getByTestId('TxtConfirmPassword');
+
+    expect(txtFirstName).toHaveTextContent('');
+    expect(txtLastName).toHaveTextContent('');
+    expect(txtEmail).toHaveTextContent('');
+    expect(txtPassword).toHaveTextContent('');
+    expect(txtConfirmPassword).toHaveTextContent('');
+  });
+
+  xtest('Test Case-4 [Testing Empty or Blank Input]', () => {
+    render(<App/>);
+    const registerButton = screen.getByTestId("RegisterButton");
+
+    fireEvent.click(registerButton);
+
+    
+  });
+
 });
+
+
+
+
+
+
+
+
+
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
